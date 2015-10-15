50541e3a6a0f08fd337215ab74603f7f5d7fcd10
Bogdan Ponomarenko <ponomarenko.bogdan@yandex.ru>
2015-10-16 01:32:18 +0300

    add file and create int color matrix

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,11 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -67,18 +72,57 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "Конвертировать";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(117, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Открыть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(420, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(574, 395);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(420, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ваше изображение:";
+            this.label3.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1006, 440);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Image2ASCII Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,6 +134,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Image2ASCII
 {
     public partial class Form1 : Form
     {
+        Bitmap newBitmap;
+        Image file;
+        bool opened = false;
+        int width, height;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFile();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int [,,] colorMatrix = getColorMatrix(width: width, height: height, image: newBitmap);
+        }
+
+        public void openFile()
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                file = Image.FromFile(openFileDialog1.FileName);
+                newBitmap = new Bitmap(openFileDialog1.FileName);
+                opened = true;
+                label3.Visible = true;
+                pictureBox1.Image = file;
+                width = file.Width;
+                height = file.Height;
+            }
+        }
+        public int[,,] getColorMatrix (int width, int height, Bitmap image)
+        {
+            int[,,] colorMatrix = new int[3, width, height];
+
+            System.IO.StreamWriter textFile = new System.IO.StreamWriter(@"C:\Users\Bogdan\YandexDisk\Development\Listings\[ITVDN]\C#\test.txt");
+
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x= 0; x < width; ++x)
+                {
+                    Color color = image.GetPixel(x, y);
+                    colorMatrix[0, y, x] = color.R;
+                    colorMatrix[1, y, x] = color.G;
+                    colorMatrix[2, y, x] = color.B;
+
+                    textFile.Write(colorMatrix[0, y, x] + ' ');
+                    textFile.Write(colorMatrix[1, y, x] + ' ');
+                    textFile.WriteLine(colorMatrix[2, y, x]);
+
+                }
+            }
+
+
+                return colorMatrix;
         }
     }
 }
